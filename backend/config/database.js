93ff753ec71ad4de353fc584b69da5c6d002b924
Pradeep-Prady <pradeepprady005@gmail.com>
2023-08-06 12:31:51 +0530

    new bug

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,11 +5,10 @@ const connectDatabase = () => {
    mongoose.connect(process.env.DB_LOCAL_URI,{
        useNewUrlParser:true,
        useUnifiedTopology:true,
-
    }).then(con => {
        console.log(`Mongodb is connected to the host ${con.connection.host} this is Narikoootam`);
-   }).catch(() => {
-    console.log("Failed to connect");
+   }).catch((e) => {
+    console.log("Failed to connect",e);
    })
 }
 
